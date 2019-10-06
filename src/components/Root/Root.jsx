0080fbb626a6f8694562ebd4b7edcfad48e0cb11
Eugen Kochtyrew <eukolo12gmail.com>
2019-10-07 00:09:04 +0200

    material-ui: first step

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -3,14 +3,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import DragHandle from '../DragHandle/DragHandle';
 import Tracker from '../Tracker/Tracker';
 import Settings from '../Settings/Settings';
 import ControlPlane from '../ControlPlane/ControlPlane';
 import Instructor from '../Instructor/Instructor';
 
-import 'bootstrap/scss/bootstrap.scss';
 import '../../scss/styles.scss';
+import 'material-design-icons/iconfont/material-icons.css';
+import 'typeface-roboto/index.css';
+import 'typeface-roboto-mono/index.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+
+// import 'bootstrap/scss/bootstrap.scss';
 
 // check window id
 const isInstructorWindow =
@@ -21,28 +27,32 @@ const Root = ({ store }) => {
   if (isInstructorWindow) {
     return (
       <Provider store={store}>
-        <DragHandle />
-        <Instructor />
+        <CssBaseline>
+          <DragHandle />
+          <Instructor />
+        </CssBaseline>
       </Provider>
     );
   }
   return (
     <Provider store={store}>
-      <Router>
-        <DragHandle />
-        <Switch>
-          <Route exact path="/">
-            <Tracker />
-          </Route>
-          <Route path="/settings">
-            <Settings />
-          </Route>
-          <Route path="/instructor">
-            <Instructor />
-          </Route>
-        </Switch>
-        <ControlPlane />
-      </Router>
+      <CssBaseline>
+        <Router>
+          <DragHandle />
+          <Switch>
+            <Route exact path="/">
+              <Tracker />
+            </Route>
+            <Route path="/settings">
+              <Settings />
+            </Route>
+            <Route path="/instructor">
+              <Instructor />
+            </Route>
+          </Switch>
+          <ControlPlane />
+        </Router>
+      </CssBaseline>
     </Provider>
   );
 };
